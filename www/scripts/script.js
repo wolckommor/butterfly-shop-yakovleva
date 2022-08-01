03b0c24632d0c8870b4773e89a1dd99a3058c4aa
Tatyana Yakovleva <tvolkomor@gmail.com>
2022-08-02 00:29:31 +0300

    + tabs

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -11,4 +11,17 @@ $(document).ready(function(){
   });
 
 
+  $('.js-tabs-link').on('click', function(){
+    $('.js-tabs-link').removeClass('active');
+    $(this).addClass('active');
+
+    let index = $(this).index('.js-tabs-link');
+
+    $('.js-tabs-content').removeClass('active');
+    $('.js-tabs-content').eq(index).addClass('active');
+
+  })
+
+
+
 })
