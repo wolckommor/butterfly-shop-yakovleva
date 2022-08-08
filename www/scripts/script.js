413d1543c860d65b1b32a4b67cfc4e63c5c5c38f
Tatyana Yakovleva <tvolkomor@gmail.com>
2022-08-09 00:44:15 +0300

    + фильтрация через дата-атрибуты

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,15 +1,72 @@
 $(document).ready(function(){
 
+  // Меню
+
   $('.js-header-burger').on('click', function(){
 
     $('.js-header-menu').slideToggle();
 
   })
 
+  // Фильтрация
+
+    // $('.js-filter-link').on('click', function() {
+
+    //   let filter = $(this).data('filter');
+
+    //   $('.js-filter-link').removeClass('active');
+    //   $(this).addClass('active');
+
+    //   if (filter === 'all') {
+
+    //     $('.js-portfolio-item').show();
+
+    //     return;
+    //   }
+
+
+    //   $('.js.portfolio-item').each(function() {
+
+    //     let type = $(this).data('type');
+
+    //     if (filter === 'type') {
+    //       $(this).show();
+    //     } else {
+    //       $(this).hide();
+    //     }
+    //   })
+    // })
+
+
+    $('.js-filter-link').on('click', function(){
+      let filter = $(this).data('filter');
+
+      $('.js-filter-link').removeClass('active');
+      $(this).addClass('active');
+
+      if (filter === 'all') {
+        $('.js.portfolio-card').show();
+
+        return;
+      }
+      $('.js.portfolio-card').each(function(){
+        let type = $(this).data('type');
+        if (filter === type) {
+          $(this).show();
+        } else {
+          $(this).hide();
+        }
+      });
+    });
+
+
+  // Слик-слайдер
 
   $('.js-rewiews-wrap').slick({
   });
 
+
+  // Табы
 
   $('.js-tabs-link').on('click', function(){
     $('.js-tabs-link').removeClass('active');
